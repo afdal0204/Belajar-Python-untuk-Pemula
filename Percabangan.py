40ef03f31ab549f6ef3d9132bdcf5687bf650abf
afdal0204 <106063255+afdal0204@users.noreply.github.com>
2023-03-16 09:55:11 +0700

    Add files via upload

---
+++ Percabangan.py
@@ -0,0 +1,14 @@
+# Pada tutorial ini kita akan belajar percabangan pada python
+# Percabangan python terdiri dari if, elif, else
+
+a = 2
+b = 3
+
+if a == b:
+    print('Hai guyss')
+    
+elif a < b:
+    print('Apa kabar?')
+    
+else:
+    print('silahkan masukkan angka telebih dahulu')
