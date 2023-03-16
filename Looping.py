d26ffc9eea6af63d602e71f3a6a7959f562732f0
afdal0204 <106063255+afdal0204@users.noreply.github.com>
2023-03-16 10:03:35 +0700

    Add files via upload

---
+++ Looping.py
@@ -0,0 +1,5 @@
+# Pada tuturial ini kita akan belajar perulangan(looping) python menggunakan for
+
+name = ['Afdal', 'Wahyi']
+for i in name:
+    print(i)
---
+++ variable.py
@@ -0,0 +1,11 @@
+# Menambahkan variable python
+
+# ini variable
+x = 4
+y = 'Afdal'
+z = 'Wahyi'
+
+# menampilkan hasil variable
+print(x)
+print(y)
+print(z)
